853857f658d6fa201d0492d545266b7b99ac0191
phuongdv <dovietphuong.ptit@gmail.com>
2018-11-28 14:45:22 +0700

    update script

--- scripts/scheduler-task.sh
+++ scripts/scheduler-task.sh
@@ -3,29 +3,28 @@
 # pid=2018-11-20 ( A(run|done)--> B(run|done) --> C(run|done))
 # sh bin/sheduler-task A http://localhost:10001 "sh bin/app.sh arg1 arg2 arg3"
 
-NAME_SERVICE=$1
-END_POINT=$2
-CMD=$3
 
+local process_name="$1"
+local service_address="$2"
+local run_command="$3"
 
-PIPELINE_STATUS=$(curl -X GET -s $END_POINT/pipeline/status)
+local pipeline_status=$(curl -sSl -X GET ${service_address}/pipeline/status)
 
-if [ $PIPELINE_STATUS == 'RUNNING' ]
+if [[ ${pipeline_status} == 'RUNNING' ]]
 then
    #echo 'Pipeline is running, select the current pipeline job...'
 
-   CURRENT_JOB_ID=$(curl -X -s GET $END_POINT/pipeline/currentJob)
+   local current_job_id=$(curl -sSL -X GET ${service_address}/pipeline/currentJob)
 
-   if [ "$NAME_SERVICE" == "$CURRENT_JOB_ID" ]
+   if [[ "${process_name}" == "${current_job_id}" ]]
    then
-        echo 'Starting job '$CURRENT_JOB_ID
+        echo 'Starting job '${current_job_id}
         # your command here
 
-        # bin/java.sh prod PushDataToClickHouseDB '$PID'
-        bash -c "$(printf 'bash -c %q' "${@:3:1}")"
+        bash -c "$(printf 'bash -c %q' "${run_command}")"
 
-        echo 'Job '$CURRENT_JOB_ID' finished'
+        echo 'Job '${current_job_id}' finished'
         # call to service
-        curl -X GET -s $END_POINT/pipeline/update/$CURRENT_JOB_ID/done
+        curl -X GET -s ${service_address}/pipeline/update/${current_job_id}/done
    fi
 fi
