c6e1e6b7919668ab3451927cfa6a8b313dd28e0e
phuongdv <dovietphuong.ptit@gmail.com>
2018-11-28 11:14:19 +0700

    update script

--- scripts/scheduler-task.sh
+++ scripts/scheduler-task.sh
@@ -7,33 +7,25 @@ NAME_SERVICE=$1
 END_POINT=$2
 CMD=$3
 
- ### for loop all service ###
-for (( ; ; ))
-do
-   echo "Pres CTRL+C to stop..."
 
-   PIPELINE_STATUS=$(curl -X GET $END_POINT/pipeline/status)
+PIPELINE_STATUS=$(curl -X GET -s $END_POINT/pipeline/status)
 
-   if [ $PIPELINE_STATUS == 'RUNNING' ]
+if [ $PIPELINE_STATUS == 'RUNNING' ]
+then
+   #echo 'Pipeline is running, select the current pipeline job...'
+
+   CURRENT_JOB_ID=$(curl -X -s GET $END_POINT/pipeline/currentJob)
+
+   if [ $NAME_SERVICE == $CURRENT_JOB_ID ]
    then
-       #echo 'Pipeline is running, select the current pipeline job...'
+        echo 'Starting job '$CURRENT_JOB_ID
+        # your command here
 
-       CURRENT_JOB_ID=$(curl -X GET $END_POINT/pipeline/currentJob)
+        # bin/java.sh prod PushDataToClickHouseDB '$PID'
+        bash -c "$(printf 'bash -c %q' "${@:3:1}")"
 
-       if [ $NAME_SERVICE == $CURRENT_JOB_ID ]
-       then
-            echo 'Starting job '$CURRENT_JOB_ID
-            # your command here
-
-            # bin/java.sh prod PushDataToClickHouseDB '$PID'
-            bash -c "$(printf 'bash -c %q' "${@:3:1}")"
-
-            echo 'Job '$CURRENT_JOB_ID' finished'
-            # call to service
-            curl -X GET -s $END_POINT/pipeline/update/$CURRENT_JOB_ID/done
-       fi
+        echo 'Job '$CURRENT_JOB_ID' finished'
+        # call to service
+        curl -X GET -s $END_POINT/pipeline/update/$CURRENT_JOB_ID/done
    fi
-
-   sleep 5m
-
-done
+fi
