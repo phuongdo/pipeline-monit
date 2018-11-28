0fb3acbe75932fd7a089d0f6bc2829e53ce7b7a1
phuongdv <dovietphuong.ptit@gmail.com>
2018-11-29 00:12:12 +0700

    update script v1.1

--- scripts/scheduler-init.sh
+++ scripts/scheduler-init.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-# sh bin/scheduler-init.sh 2018-11-20 'A>B>C' http://localhost:10001
+# sh bin/scheduler-init.sh demography-pred-raw 2018-11-20 'A>B>C' http://localhost:10001
 #
 PNAME=$1
 PID=$2
 BODY_DATA=$3
 END_POINT=$4
 
-PIPELINE_STATUS=$(curl -X GET -s $END_POINT/pipeline/status)
+PIPELINE_STATUS=$(curl -X GET -s $END_POINT/pipeline/$PNAME/status)
 if [ $PIPELINE_STATUS == 'null' ]
 then
    echo 'Pipeline init...'
-   curl -X POST -s -H "Content-Type: application/json" -d $BODY_DATA $END_POINT/pipeline/create/$PID
+   curl -X POST -s -H "Content-Type: application/json" -d $BODY_DATA $END_POINT/pipeline/$PNAME/create/$PID
 fi
--- scripts/scheduler-task.sh
+++ scripts/scheduler-task.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
 # pid=2018-11-20 ( A(run|done)--> B(run|done) --> C(run|done))
-# sh bin/sheduler-task A http://localhost:10001 "sh bin/app.sh arg1 arg2 arg3"
+# sh bin/sheduler-task  A http://localhost:10001 "sh bin/app.sh arg1 arg2 arg3"
 
 function start_task {
-    local process_name="$1"
-    local service_address="$2"
-    local run_command="$3"
+    local pipeline_name="$1"
+    local process_name="$2"
+    local service_address="$3"
+    local run_command="$4"
 
-    local pipeline_status=$(curl -sSl -X GET ${service_address}/pipeline/status)
+    local pipeline_status=$(curl -sSl -X GET ${service_address}/pipeline/${pipeline_name}/status)
 
     if [[ ${pipeline_status} == 'RUNNING' ]]
     then
        #echo 'Pipeline is running, select the current pipeline job...'
 
-       local current_job_id=$(curl -sSL -X GET ${service_address}/pipeline/currentJob)
+       local current_job_id=$(curl -sSL -X GET ${service_address}/pipeline/${pipeline_name}/currentJob)
 
        if [[ "${process_name}" == "${current_job_id}" ]]
        then
@@ -25,7 +26,7 @@ function start_task {
 
             echo 'Job '${current_job_id}' finished'
             # call to service
-            curl -X GET -s ${service_address}/pipeline/update/${current_job_id}/done
+            curl -X GET -s ${service_address}/pipeline/${pipeline_name}/update/${current_job_id}/done
        fi
     fi
 }
